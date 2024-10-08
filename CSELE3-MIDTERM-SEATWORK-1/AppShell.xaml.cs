03bafcd5c9b4c7dd057968901a2a4d8b9bab3bf5
Timy Villarmia <timyvillarmia@gmail.com>
2024-10-08 21:29:05 +0800

    wip

---
+++ CSELE3-MIDTERM-SEATWORK-1/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CSELE3_MIDTERM_SEATWORK_1
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CSELE3-MIDTERM-SEATWORK-1/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace CSELE3_MIDTERM_SEATWORK_1
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
